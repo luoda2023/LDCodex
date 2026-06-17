916d67d691cd10aeb90fc6be72747f9064baa796
luoda2024 <info@dicad.cn>
2026-06-18 00:52:13 +0800

    chore: 移除临时脚本

--- fix_toml.py
+++
@@ -1,6 +0,0 @@
-﻿import os; os.chdir('J:/codex-work/LDCodex2')
-toml = open('Cargo.toml', 'r', encoding='utf-8').read()
-# Restore to 2021
-toml = toml.replace('edition = "2024"', 'edition = "2021"')
-open('Cargo.toml', 'w', encoding='utf-8').write(toml)
-open('Cargo.toml', 'r', encoding='utf-8').read().split('\n')[11]
